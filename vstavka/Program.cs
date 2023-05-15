d4efa79f51a3c0dccf764fba26f29a117f22d0d4
Tanyhaa <tanykit2001@gmail.com>
2023-05-15 15:06:48 +0300

    proga

---
+++ vstavka/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace vstavka
+{//сортировка вставками
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите размер массива:");
+                int size = Convert.ToInt32(Console.ReadLine());
+                var arr = new int[size];
+
+                Console.WriteLine("Ввод массива");
+                for (int i = 0; i < size; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int j = 0, k = 0;
+
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    k = arr[i];
+                    j = i - 1;
+
+                    while (j >= 0 && arr[j] > k)
+                    {
+                        arr[j + 1] = arr[j];
+                        arr[j] = k;
+                        j--;
+                    }
+
+                }
+
+                Console.Write("Новый массив: ");
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+    
+}
---
+++ vybor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace vybor
+{
+    internal class Program
+    {//сортировка выбором
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            var arr = new int[size];
+
+            Console.WriteLine("Enter array:");
+                for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int min = 0, j = 0, index = 0, swap = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                min = int.MaxValue;
+                while (j < size)
+                {
+                    if (arr[j] < min)
+                    {
+                        min = arr[j];
+                        index = j;
+                    }
+                    j++;
+                }
+                j = i + 1;
+                swap = arr[i];
+                arr[i] = min;
+                arr[index] = swap;
+
+            }
+
+            Console.WriteLine("New array:");
+            foreach (var a in arr)
+            {
+                Console.Write(a + " ");
+            }
+
+        }
+    }
+        
+    
+}
